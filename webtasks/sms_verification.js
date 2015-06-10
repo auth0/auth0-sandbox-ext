82478d63de164e54322f032a4639aeb199b5236c
Tomasz Janczuk <tomasz@janczuk.org>
2015-06-10 16:10:32 -0700

    sms verification

--- webtasks/sms_verification.js
+++ webtasks/sms_verification.js
@@ -7,11 +7,51 @@ return function (ctx, cb) {
         return cb(new Error('Missing `phone` parameter.'));
 
     if (ctx.data.verification_code) {
+
         // verify user, return profile
 
-        return cb(new Error('Not supported yet'));
+        request({
+            url: 'https://webtask-cli.auth0.com/oauth/ro',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            timeout: 20000,
+            body: JSON.stringify({
+                client_id: 'pyp3Ee2t0vhpC3cB1TUUCtRQv3qdEoTe',
+                username: ctx.data.phone,
+                password: ctx.data.verification_code,
+                connection: 'sms',
+                grant_type: 'password',
+                scope: 'openid profile'
+            })
+        }, function (error, res, body) {
+            if (error) {
+                console.log('Failed to verify SMS code', ctx.data.phone, 'with error', error);
+                return cb(error);
+            }
+            if (res.statusCode < 200 || res.statusCode > 299) {
+                var msg = 'Unable to verify SMS code sent to ' 
+                    + ctx.data.phone + ' with HTTP status ' + res.statusCode 
+                    + ' and body ' + body;
+                console.log(msg);
+                return cb(new Error(msg));
+            }
+            try {
+                body = JSON.parse(body);
+            }
+            catch (e) {
+                var msg = 'Failed to verify SMS code sent to ' + ctx.data.phone 
+                    + ' with response body ' + body;
+                console.log(msg);
+                return cb(new Error(msg));
+            }
+            console.log('Successful SMS verification ', ctx.data.phone);
+            cb(null, body);
+        }
     }
     else {
+
         // initiate SMS verification
 
         request({
