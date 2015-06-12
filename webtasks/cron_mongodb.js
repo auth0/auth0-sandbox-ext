03fe948ac49389cbddb769d4887f66131ba8d782
root <ggoodman@gmail.com>
2015-06-12 13:40:14 +0000

    Add endpoints and logic for execution and logging

--- webtasks/cron_mongodb.js
+++ webtasks/cron_mongodb.js
@@ -10,44 +10,50 @@ var valid_actions = {
     list_jobs: 'GET',
     job_history: 'GET',
     reserve_jobs: 'POST',
-    renew_reservation: 'PUT',
-    release_reservation: 'DELETE',
+    update_job: 'PUT',
 };
 
 
-var webtask = function (context, req, res) {
+module.exports = function (context, req, res) {
     var action = validate_method(valid_actions);
+    var now = new Date();
     
-    if (!validate_params(['MONGO_COLLECTION']))
+    if (!validate_params(['JOB_COLLECTION', 'LOG_COLLECTION', 'cluster_url']))
         return;
     
     if (action === 'put_job') {
         // Create or update a scheduled webtask
         
-        if (!validate_params(['container', 'name', 'schedule', 'token']))
+        if (!validate_params(['container', 'name']))
+            return;
+        
+        if (!validate_body(['schedule', 'token']))
             return;
             
-        var now = new Date();
-        
-        var item = {
-            state: 'active',
-            schedule: context.data.schedule,
-            token: context.data.token,
-            container: context.data.container,
-            name: context.data.name,
-            last_run_at: null,
-            next_available_at: now,
-            last_result: null,
-            run_count: 0,
-            error_count: 0,
+        var update = {
+            $set: {
+                state: 'active',
+                schedule: context.body.schedule,
+                cluster_url: context.data.cluster_url,
+                token: context.body.token,
+                container: context.data.container,
+                name: context.data.name,
+                last_scheduled_at: now,
+                next_available_at: now,
+            },
+            $setOnInsert: {
+                last_result: null,
+                run_count: 0,
+                error_count: 0,
+            }
         };
         
-        return withMongoCollection(context.data.MONGO_COLLECTION)
+        return withMongoCollection(context.data.JOB_COLLECTION)
             .then(function (coll) {
                 return coll.findOneAndUpdateAsync({
-                    container: item.container,
-                    name: item.name,
-                }, item, {
+                    container: context.data.container,
+                    name: context.data.name,
+                }, update, {
                     returnOriginal: false,
                     upsert: true,
                 });
@@ -67,10 +73,9 @@ var webtask = function (context, req, res) {
         if (!validate_params(['count', 'ttl']))
             return;
         
-        var now = new Date();
-        var next_available_at = new Date(now.valueOf() + (parseInt(context.data.ttl, 10) * 1000))
+        var next_available_at = new Date(now.valueOf() + (parseInt(context.data.ttl, 10) * 1000));
         
-        return withMongoCollection(context.data.MONGO_COLLECTION)
+        return withMongoCollection(context.data.JOB_COLLECTION)
             .then(function (coll) {
                 return Bluebird.resolve(_.range(context.data.count))
                     // TODO (ggoodman): Refactor this to use reduce to avoid calling N queries if reservation fails before N
@@ -93,7 +98,7 @@ var webtask = function (context, req, res) {
                         })
                             .get('value'); // Only pull out the value
                         
-                    }, {concurrency: 1}) // Serial mapping
+                    })
                     // Don't prevent one failure from blocking the entire
                     // reservation (that would mean extra waiting on those that
                     // didn't fail)
@@ -124,7 +129,7 @@ var webtask = function (context, req, res) {
     } else if (action === 'list_jobs') {
         // List all scheduled webtasks or only those in a container passed via query
         
-        return withMongoCollection(context.data.MONGO_COLLECTION)
+        return withMongoCollection(context.data.JOB_COLLECTION)
             .then(function (coll) {
                 var query = {};
                 
@@ -152,7 +157,7 @@ var webtask = function (context, req, res) {
         if (!validate_params(['container', 'name']))
             return;
             
-        return withMongoCollection(context.data.MONGO_COLLECTION)
+        return withMongoCollection(context.data.JOB_COLLECTION)
             .then(function (coll) {
                 var query = {
                     container: context.data.container,
@@ -176,7 +181,7 @@ var webtask = function (context, req, res) {
         if (!validate_params(['container', 'name']))
             return;
             
-        return withMongoCollection(context.data.MONGO_COLLECTION)
+        var deleteJob = withMongoCollection(context.data.JOB_COLLECTION)
             .then(function (coll) {
                 var filter = {
                     container: context.data.container,
@@ -184,10 +189,125 @@ var webtask = function (context, req, res) {
                 };
                 
                 return coll.deleteOne(filter, {w: 1});
-            })
+            });
+            
+        var deleteLogs = withMongoCollection(context.data.LOG_COLLECTION)
+            .then(function (coll) {
+                var filter = {
+                    container: context.data.container,
+                    name: context.data.name,
+                };
+                
+                return coll.deleteMany(filter, {w: 1});
+            });
+        
+        Bluebird.all([deleteJob, deleteLogs])
             .then(function (data) {
                 res.writeHead(204);
                 res.end();
+            })
+            .catch(function (err) {
+                error(err.statusCode || 500, err);
+            });
+    
+    } else if (action === 'update_job') {
+        // Update a single scheduled webtask based on criteria
+        
+        if (!validate_params(['container', 'name']))
+            return;
+        
+        if (!validate_body(['criteria', 'updates']))
+            return;
+            
+        if (!_.isObject(context.body.criteria)) return error(400, 'Expecting criteria to be an object');
+        if (!_.isObject(context.body.updates)) return error(400, 'Expecting updates to be an object');
+        
+        var criteria = context.body.criteria;
+        var updates = context.body.updates;
+        var result = updates.last_result;
+        
+        var makeMongoDates = function (obj, fields) {
+            _.forEach(fields, function (field) {
+                if (obj[field]) obj[field] = new Date(obj[field]);
+            });
+        };
+        
+        makeMongoDates(criteria, ['last_scheduled_at', 'next_available_at']);
+        makeMongoDates(updates, ['last_scheduled_at', 'next_available_at']);
+            
+        // This update includes a last_result, so we will add it to our log
+        if (result) {
+            // Fire and forget this as considered non-critical
+            withMongoCollection(context.data.LOG_COLLECTION)
+                .then(function (coll) {
+                    var logEntry = _.extend(result, {
+                        container: context.data.container,
+                        name: context.data.name,
+                        created_at: new Date(),
+                    });
+                    
+                    return coll.insertOneAsync(logEntry, {w: 1});
+                });
+        }
+            
+        return withMongoCollection(context.data.JOB_COLLECTION)
+            .then(function (coll) {
+                var filter = _.defaults({
+                    container: context.data.container,
+                    name: context.data.name,
+                }, context.body.critera);
+                
+                var update = {
+                    $set: context.body.updates,
+                };
+                
+                // Track stats based on result
+                if (result) {
+                    if (result.type == 'success') update.$inc = { run_count: 1};
+                    if (result.type == 'error') update.$inc = { error_count: 1};
+                }
+                
+                return coll.findOneAndUpdateAsync(filter, update)
+                    .then(stripMongoId);
+            })
+            .then(function (data) {
+                // There may be no jobs that match criteria, resulting in 
+                // data being null
+                
+                if (!data) throw {
+                    statusCode: 404,
+                    message: 'No job matched the supplied criteria',
+                };
+                
+                res.writeHead(200);
+                res.end(JSON.stringify(data));
+            })
+            .catch(function (err) {
+                error(err.statusCode || 500, err);
+            });
+    } else if (action === 'job_history') {
+        // List all scheduled webtask's result history
+        
+        if (!validate_params(['container', 'name']))
+            return;
+        
+        return withMongoCollection(context.data.LOG_COLLECTION)
+            .then(function (coll) {
+                var query = {
+                    container: context.data.container,
+                    name: context.data.name,
+                };
+                
+                // TODO (ggoodman): Pagination logic
+                return coll.findAsync(query)
+                    .then(function (cursor) {
+                        return Bluebird.promisify(cursor.toArray, cursor)();
+                    });
+            })
+            .map(stripMongoId)
+            .then(function (data) {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(data));
             })
             .catch(function (err) {
                 error(err.statusCode || 500, err);
@@ -203,7 +323,10 @@ var webtask = function (context, req, res) {
     // Helper methods
     
     function stripMongoId (doc) {
-        if (doc && doc._id) delete doc._id;
+        if (doc) {
+            if (doc._id) delete doc._id;
+            if (doc.last_result && doc.last_result._id) delete doc.last_result._id;
+        }
         
         return doc;
     }
@@ -243,7 +366,16 @@ var webtask = function (context, req, res) {
     function validate_params(required_params) {
         for (var i in required_params) {
             if (typeof context.data[required_params[i]] !== 'string') {
-                return error(400, 'Missing ' + required_params[i] + '.');
+                return error(400, 'Missing query parameter ' + required_params[i] + '.');
+            }
+        }
+        return true;
+    }
+
+    function validate_body(required_fields) {
+        for (var i in required_fields) {
+            if (!context.body[required_fields[i]]) {
+                return error(400, 'Missing payload parameter ' + required_fields[i] + '.');
             }
         }
         return true;
@@ -256,5 +388,3 @@ var webtask = function (context, req, res) {
         return false;
     }
 };
-
-return webtask;
