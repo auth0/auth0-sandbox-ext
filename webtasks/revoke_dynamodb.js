0d42c9921ba129298822fefc35f6420d1578f58b
Tomasz Janczuk <tomasz@janczuk.org>
2015-10-13 13:17:17 -0700

    more logging in revocation webtask

--- webtasks/revoke_dynamodb.js
+++ webtasks/revoke_dynamodb.js
@@ -16,7 +16,7 @@ return function (context, req, res) {
         if (!validate_params(['ACCESS_KEY_ID', 'SECRET_ACCESS_KEY', 'REGION', 'TABLE']))
             return;
 
-        console.log('Creating DynamoDB client', { region: context.data.REGION });
+        console.log('Creating DynamoDB client', { req_id: context.id, region: context.data.REGION });
 
         var aws = require('aws-sdk');
         aws.config.accessKeyId = context.data.ACCESS_KEY_ID;
@@ -33,6 +33,12 @@ return function (context, req, res) {
 
         if (!validate_params(['jti','jwt']))
             return;
+
+        console.log('Revoking a token', {
+            req_id: context.id,
+            jwt: context.data.jwt
+        });
+
         dynamo.putItem({
             Item: {
                 jti: { S: context.data.jti },
@@ -42,11 +48,17 @@ return function (context, req, res) {
         }, function (err) {
             if (err) {
                 console.log('Error revoking a token', {
+                    req_id: context.id,
                     error: err.stack || error,
                     jwt: context.data.jwt
                 });
                 return error(502);
             }
+
+            console.log('Revoked a token', {
+                req_id: context.id
+            });
+
             res.writeHead(200);
             return res.end();
         });
@@ -58,6 +70,11 @@ return function (context, req, res) {
         if (!validate_params(['jti']))
             return;
 
+        console.log('Checking token revocation', {
+            req_id: context.id,
+            jti: context.data.jti
+        });
+
         dynamo.query({
             KeyConditions: {
                 jti: { 
@@ -68,12 +85,19 @@ return function (context, req, res) {
         }, function (err, data) {
             if (err) {
                 console.log('Error checking token revocation', {
+                    req_id: context.id,
                     error: err.stack || err,
-                    jwt: context.data.jwt
+                    jti: context.data.jti
                 });
                 return error(502);
             }
-            res.writeHead(data.Count > 0 ? 200 : 404);
+            var status = data.Count > 0 ? 200 : 404;
+            console.log('Checked token revocation', {
+                req_id: context.id,
+                jti: context.data.jti,
+                status_code: status
+            });
+            res.writeHead(status);
             return res.end();
         });
     }
@@ -88,7 +112,7 @@ return function (context, req, res) {
     }
 
     function error(code, err) {
-        console.log(code + (err ? (': ' + err) : ''));
+        console.log(code + (err ? (': ' + err) : ''), { req_id: context.id });
         res.writeHead(code);
         res.end(err);
         return false;
